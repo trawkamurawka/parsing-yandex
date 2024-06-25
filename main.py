e8e9f7981d542ce934cf57c9b40f45944be6b783
Anya Sazhko <ice_age7@mail.ru>
2024-06-25 16:08:58 +0300

    added menu

--- main.py
+++ main.py
@@ -2,20 +2,53 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+import pprint
 
 load_dotenv()
-API_KEY = os.getenv('API_KEY')
-BASE_URL = os.getenv('BASE_URL')
+API_KEY: str = os.getenv('API_KEY')
+BASE_URL: str = 'https://dictionary.yandex.net/api/v1/dicservice.json'
 
 
 def get_langs():
-    response = requests.get('{base_url}/lookup?key={key}&lang=ru-en&text=задача'.format(
-        base_url=BASE_URL,
-        key=API_KEY
-    ))
+    response = requests.get('{base_url}/getLangs'.format(base_url=BASE_URL),
+                            params={'key': API_KEY})
 
     return response
 
 
-result = json.loads(get_langs().text)
-print(result)
+def lookup(land: str, text: str, ui='ru'):
+    response = requests.get('{base_url}/lookup'.format(base_url=BASE_URL),
+                            params={'key': API_KEY, 'lang': land, 'text': text, 'ui': ui})
+
+    return response
+
+
+def menu():
+    print('Добро пожаловать в словарь!\n')
+    langs_response = get_langs()
+
+    if langs_response.status_code != 200:
+        print('Извините, не удалось получить список направлений перевода.\nПопробуйте воспользоваться позже.')
+        exit(1)
+
+    langs = langs_response.json()
+    print('Выберите одно из доступных направлений перевода:')
+    for index, land in enumerate(langs):
+        if index % 10 == 0:
+            print('\n')
+        print(land, end=' ')
+
+    while (lang := input('\n\nВведите направление: ')) not in langs:
+        print('Такого направления нет. Попробуйте ещё раз')
+
+    text = input('Введите слово или фразу для перевода: ')
+    lookup_response = lookup(lang, text)
+    if lookup_response.status_code != 200:
+        print('Не удалось выполнить перевод:', lookup_response.text)
+        exit(1)
+
+    pprint.pprint(lookup_response.json())
+
+
+if __name__ == '__main__':
+    menu()
